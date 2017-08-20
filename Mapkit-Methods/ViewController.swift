2b1604a27d340b58dc27ecee6b55060504e43394
Alex Beattie <artisanbranding@me.com>
2017-08-20 11:21:59 -0700

    Initial Commit

---
+++ Mapkit-Methods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mapkit-Methods
+//
+//  Created by Alex Beattie on 8/20/17.
+//  Copyright © 2017 Artisan Branding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
